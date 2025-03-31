46aa21cdcb5c6a84ee5c6202fb22e7f871dc1f5a
telepnevs228 <maksimtelepnev2@gmail.com>
2025-03-31 14:33:49 +0300

    metode izmaina cikls

--- src/kaulins.java
+++ src/kaulins.java
@@ -2,18 +2,19 @@ import java.util.Random;
 
 public class kaulins {
 
-	static void mestKaulinu() {
+	static void mestKaulinu(int reizes) {
 		
 		int skaitlis;
 		Random rand = new Random();
+		for (int i =0; i<reizes; i++) {
 		skaitlis = rand.nextInt(6)+1;
 		System.out.println("Uzkrita skaitlis: "+skaitlis);
 		
-		
+		}
 	}
 	
 	public static void main(String[] args) {
-		mestKaulinu();
+		mestKaulinu(3);
 	}
 
 }
