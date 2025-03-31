c6cb4e4422aed17a1848b899129cf14f8ab69290
telepnevs228 <maksimtelepnev2@gmail.com>
2025-03-31 14:29:26 +0300

    pievienota metode

--- src/kaulins.java
+++ src/kaulins.java
@@ -2,13 +2,18 @@ import java.util.Random;
 
 public class kaulins {
 
-	public static void main(String[] args) {
+	static void mestKaulinu() {
+		
 		int skaitlis;
 		Random rand = new Random();
 		skaitlis = rand.nextInt(6)+1;
 		System.out.println("Uzkrita skaitlis: "+skaitlis);
-		System.out.println("Uzkrita skaitlis: "+skaitlis);
-		System.out.println("Uzkrita skaitlis: "+skaitlis);
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		mestKaulinu();
 	}
 
 }
