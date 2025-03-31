48e2e2ca3bb23ecfec3da57c3fbb24fab31b359e
telepnevs228 <maksimtelepnev2@gmail.com>
2025-03-31 14:13:55 +0300

    pirma versija

--- src/kaulins.java
+++ src/kaulins.java
@@ -1,9 +1,14 @@
+import java.util.Random;
 
 public class kaulins {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int skaitlis;
+		Random rand = new Random();
+		skaitlis = rand.nextInt(6)+1;
+		System.out.println("Uzkrita skaitlis: "+skaitlis);
+		System.out.println("Uzkrita skaitlis: "+skaitlis);
+		System.out.println("Uzkrita skaitlis: "+skaitlis);
 	}
 
 }
